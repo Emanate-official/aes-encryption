28fbf00d0b4268259271b81a3f9d76cb6dd2a43e
Blake Wood Jr <blake.wood.bst@visible.edu>
2021-10-21 16:20:15 -0700

    added random passphrase generation sha256 hash. updated version to 1.0.0

--- lib/aes_encryption.dart
+++ lib/aes_encryption.dart
@@ -12,9 +12,15 @@ part 'src/decrypt_aes.dart';
 part 'src/derive_key_iv.dart';
 part 'src/uint8list_from_string.dart';
 part 'src/get_random_w_nonzero.dart';
+part 'src/generate_passphrase.dart';
+part 'src/generate_random_number.dart';
+part 'src/generate_random_string.dart';
 
-/// A Calculator.
 class AesEncryption {
+  /// Encrypts any String to AES Encrypted String
   static String encryptString(String string, String passphrase) => encryptAES(string ,passphrase);
+  /// Decrypts any AES Encrypted String back to a String
   static String decryptString(String encryptedString, String passphrase) => decryptAES(encryptedString, passphrase);
+  /// Generated a random SHA256 Hash String for use as a Passphrase
+  static Future<String> generatePassphrase() async => await generateRandomPassphrase();
 }
